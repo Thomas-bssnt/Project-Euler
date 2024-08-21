a32cf8a37f65071bea61ddc652051a4fb320fa90
Thomas Bassanetti <49919330+Thomas-bssnt@users.noreply.github.com>
2024-08-21 13:10:44 +0200

    Improve problem 34

--- problems/34.py
+++ problems/34.py
@@ -16,10 +16,9 @@ def solution():
     factorials = {str(number): factorial(number) for number in range(10)}
 
     # A number with at least n digits will be always greater than the sum of the factorial of its digits.
-    n = 0
-    while sum(factorials["9"] for _ in range(n)) >= 10 ** n - 1:
+    n = 1
+    while factorials["9"] * n >= 10 ** n - 1:
         n += 1
-    n += 1
 
     return sum(
         number
