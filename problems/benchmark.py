2be52215cec4010c1f0235c4280c7ae88e9fd9ef
Thomas Bassanetti <49919330+Thomas-bssnt@users.noreply.github.com>
2024-10-04 15:51:57 +0200

    Add the total time in benchmark.py

--- problems/benchmark.py
+++ problems/benchmark.py
@@ -6,6 +6,8 @@ from importlib import import_module
 if __name__ == "__main__":
     directory = Path(".")
 
+    total_time = 0
+
     for file_path in sorted(directory.glob("*.py")):
         if not match(r"\d{2}", file_path.stem):
             continue
@@ -16,8 +18,9 @@ if __name__ == "__main__":
         result = module.solution()
         end_time = default_timer()
 
-        print(
-            f"Problem {file_path.stem}: "
-            f"result = {result} "
-            f"({(end_time - start_time) * 1000:.3f}ms)"
-        )
+        time = end_time - start_time
+        total_time += time
+
+        print(f"Problem {file_path.stem}: result = {result} ({time * 1000:.3f}ms)")
+
+    print(f"Total time: {total_time:.3f}s")
